d17634c8f2b8568e5295406cfd2b090075708ca4
Nihal <nihalmohammad705@gmail.com>
2026-09-06 11:27:24 +0530

    feat(pages): add HomePage layout combining all sections

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import Hero from '../components/sections/Hero'
+import About from '../components/sections/About'
+import Projects from '../components/sections/Projects'
+import Skills from '../components/sections/Skills'
+import Contact from '../components/sections/Contact'
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </>
+  )
+}
+
+export default HomePage
