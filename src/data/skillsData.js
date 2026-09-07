33ef8931698d08c668e210bbe9f5d0318ac35068
Nihal <nihalmohammad705@gmail.com>
2026-09-07 19:25:21 +0530

    feat(data): add skills data for frontend and backend technologies

---
+++ src/data/skillsData.js
@@ -0,0 +1,73 @@
+// frontend/src/data/skillsData.js
+export const skillsData = [
+  { 
+    name: 'Java', 
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg', 
+    color: '#007396'
+  },
+  { 
+    name: 'Python', 
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg', 
+    color: '#3776AB'
+  },
+  { 
+    name: 'React', 
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg', 
+    color: '#61DAFB'
+  },
+  { 
+    name: 'FastAPI', 
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/fastapi/fastapi-original.svg', 
+    color: '#009688'
+  },
+  { 
+    name: 'PostgreSQL', 
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg', 
+    color: '#336791'
+  },
+  { 
+    name: 'Git', 
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg', 
+    color: '#F05032'
+  },
+  { 
+    name: 'Tailwind CSS', 
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg', 
+    color: '#06B6D4'
+  },
+  { 
+    name: 'JavaScript', 
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg', 
+    color: '#F7DF1E'
+  },
+  { 
+    name: 'Docker', 
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg', 
+    color: '#2496ED'
+  },
+  { 
+    name: 'HTML5', 
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg', 
+    color: '#E34F26'
+  },
+  { 
+    name: 'CSS3', 
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg', 
+    color: '#1572B6'
+  },
+  { 
+    name: 'MySQL', 
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg', 
+    color: '#4479A1'
+  },
+  { 
+    name: 'MongoDB', 
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg', 
+    color: '#47A248'
+  },
+  { 
+    name: 'Node.js', 
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg', 
+    color: '#339933'
+  },
+]
