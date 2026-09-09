59e725eb9342eebc768ff36c20ee92fe5994f12d
Nihal <nihalmohammad705@gmail.com>
2026-09-09 14:52:20 +0530

    feat(app): add routing with error boundary and preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import Navbar from './components/common/Navbar'
 import HomePage from './pages/HomePage'
 import Footer from './components/common/Footer'
+import BackToTop from './components/common/BackToTop'
 
 function App() {
   return (
-    <Router>
+    <Router
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
       <div className="min-h-screen bg-dark-200 text-white">
         <Navbar />
         <main>
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </main>
         <Footer />
+        <BackToTop /> 
         <Toaster 
           position="top-right" 
           toastOptions={{ 
